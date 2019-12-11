dd61ddbec635a7527baa951285ffbbbec3a3cc4a
kato <moeruyasai@gmail.com>
2019-12-11 05:31:02 +0000

    add

--- kadai-4/index.js
+++ kadai-4/index.js
@@ -1,3 +1,17 @@
-function myFunction(arg1, arg2){
-    
+const product = (a,b) => {
+    console.log(a*b)
 }
+
+const sum = (n) => {
+    let result=0
+    for (i=1; i<=n; i=i+1){
+        result=result+1
+    }
+    console.log(sum2)
+}
+
+
+product(107,109)
+sum(10)
+
+//まだ
