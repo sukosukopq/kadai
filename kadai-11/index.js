f6664b4409dffa09296503dec8cd76406ffadd47
kat <moeruyasai@gmail.com>
2020-01-15 05:44:56 +0000

    追加

--- kadai-11/index.js
+++ kadai-11/index.js
@@ -1,8 +1,20 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-    //リクエストを確認
-    console.log(req.url)
-    res.write('domo')
+    
+    const num = Math.random()
+
+    // CROSヘッダーを返す
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    //ここにおみくじ貼る
+        if(num < 0.5) {
+            res.write(`吉です`)
+        } else if (num < 0.8) {
+            res.write(`中吉です`)
+        } else {
+            res.write(`大吉です`)
+        }
+    
     res.end()
 })
 server.listen(4000)
